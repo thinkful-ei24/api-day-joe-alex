aa5df8fd7626343e88fb03f638783ab565a54457
Alex <0alexgutierrez@gmail.com>
2018-08-29 08:52:56 -0700

    finished fetch items

--- scripts/api.js
+++ scripts/api.js
@@ -1,9 +1,24 @@
-const api = function() {
-	const BASE_URL = 'https://thinkful-list-api.herokuapp.com/joealex';
+const api = (function() {
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/joealex';
 
-	const getItems = callback => callback('callback is working');
+  const getItems = callback => {
+    $.getJSON(`${BASE_URL}/items`, callback);
+  };
 
-	return {
-		getItems
-	};
-} ();
+  const createItem = (name, callback) => {
+    let newItem = JSON.stringify({
+      name: name
+    });
+    $.ajax({
+      url: `${BASE_URL}/items`,
+      method: 'POST',
+      contentType: 'application/json',
+      data: newItem,
+      success: callback
+    });
+  };
+  return {
+    getItems,
+    createItem
+  };
+})();
--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,10 @@
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+  api.getItems(items => {
+    console.log(items);
+
+    items.forEach(item => store.addItem(item));
+    shoppingList.render();
+  });
 });
-
-store.items.push(Item.create('apples'));
-
-api.getItems(function(data) {
-  console.log(data);
-});
-
-console.log(api.BASE_URL);
